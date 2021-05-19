15a623f6f043d02c14b11f6bc6733af2fb91a9d2
吳俊緯 <wcw850710@gmail.com>
2021-05-19 10:25:31 +0800

    feat(algorithm/queue): 基本隊列

--- algorithm/src/queue/index.ts
+++ algorithm/src/queue/index.ts
@@ -1,4 +1,14 @@
 `
 隊列 queue
 就是先進先出的數據結構
+
+enqueue 入隊(從後入)
+dequeue 出隊(從前出)
+
+js沒有queue所以用array模擬
 `
+const queue = []
+queue.push(1) // 模擬 enqueue
+queue.push(2)
+const item1 = queue.shift() // 模擬 dequeue
+const item2 = queue.shift()
