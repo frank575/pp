d2542cc868cdbc37927bd4de23d740ae5ccf7dd3
吳俊緯 <wcw850710@gmail.com>
2021-05-19 10:45:31 +0800

    feat(algorithm/queue/recent-counter): 完成(爛題目= =)

--- algorithm/src/queue/index.ts
+++ algorithm/src/queue/index.ts
@@ -4,7 +4,6 @@
 
 enqueue 入隊(從後入)
 dequeue 出隊(從前出)
-
 js沒有queue所以用array模擬
 `
 const queue = []
@@ -12,3 +11,9 @@ queue.push(1) // 模擬 enqueue
 queue.push(2)
 const item1 = queue.shift() // 模擬 dequeue
 const item2 = queue.shift()
+
+;`
+什麼場景使用隊列
+需要先進先出的場景：
+排隊買飯、JS異步中的任務隊列、進算最近請求次數等
+`
