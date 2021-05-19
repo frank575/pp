776791a5bb21bc0fd0580d5a5dbc52f96d0fb7d8
吳俊緯 <wcw850710@gmail.com>
2021-05-20 07:40:17 +0800

    feat(*): 重整專案結構

--- algorithm/main.ts
+++
@@ -1,9 +0,0 @@
-// import './src/tree'
-import './src/linked-list'
-// import './src/queue'
-// import './src/stack'
-import './style.css'
-
-document.querySelector('#app')!.innerHTML = `
-  <h1>Hello Algorithm!</h1>
-`
--- algorithm/src/tree/binary-tree/non-recursive/index.ts
+++
@@ -1,3 +0,0 @@
-import './postorder'
-import './inorder'
-import './preorder'
