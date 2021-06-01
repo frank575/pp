72bafe92024b9dc0f958a10f48fd9bebf65d5555
吳俊緯 <wcw850710@gmail.com>
2021-06-01 08:39:12 +0800

    定義模型結構(單層)

--- javascript/tensorflow/linear-regression/index.js
+++ javascript/tensorflow/linear-regression/index.js
@@ -4,9 +4,15 @@ import * as tfvis from '@tensorflow/tfjs-vis'
 window.onload = () => {
   const xs = [1, 2, 3, 4]
   const ys = [1, 3, 5, 7]
+
   tfvis.render.scatterplot(
     {name: '線性回歸訓練集'},
     {values: xs.map((x,i)=>({x,y:ys[i]}))},
     {xAxisDomain: [0, 5], yAxisDomain: [0, 8]}
   )
+
+  // sequential 創造連續模型
+  const model = tf.sequential()
+  // units神經元個數
+  model.add(tf.layers.dense({units:1, inputShape:[1]}))
 }
