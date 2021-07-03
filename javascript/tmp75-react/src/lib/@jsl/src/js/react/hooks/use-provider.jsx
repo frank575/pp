eecc93c315567531ad68f7a4359f42c0c8f27199
吳俊緯 <wcw850710@gmail.com>
2021-07-03 12:06:41 +0800

    feat(@jsl) [broken] v1 移除inject傳入字串的使用方式

--- javascript/tmp75-react/src/lib/@jsl/src/js/react/hooks/use-provider.jsx
+++ javascript/tmp75-react/src/lib/@jsl/src/js/react/hooks/use-provider.jsx
@@ -1,22 +1,22 @@
 /// 提供者&供給者
 /// author frank575
+/// v1 [broken] 移除inject傳入字串的使用方式
 /// v0
-
 
 import { createContext, useContextSelector } from 'use-context-selector'
 
 /**
  * @template T, S
  * @param {function(): S} providerService
- * @return {{inject: (function(callback: function(state: S): T): T | string), Provider: function({children: *}): *}}
+ * @return {{inject: (function(callback: function(state: S): T): T), Provider: function({children: *}): *}}
  */
 export const useProvider = providerService => {
 	const Context = createContext(null)
 	const Provider = ({ children }) => (
 		<Context.Provider value={providerService()}>{children}</Context.Provider>
 	)
-	const inject = (getter = state => undefined) =>
-		useContextSelector(Context, typeof getter === 'string' ? state => state[getter] : getter)
+	const inject = (getter = () => undefined) =>
+		useContextSelector(Context, getter)
 
 	return {
 		Provider,
