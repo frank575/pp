cda9406cee1aca9fd2c6ad9bc8f6a5a310bce979
吳俊緯 <wcw850710@gmail.com>
2021-07-04 22:11:31 +0800

    refactor(billboard) service代碼邏輯拆分

---
+++ javascript/tmp75-react/src/pages/billboard/use-list.js
@@ -0,0 +1,92 @@
+import { useStore } from '@/core/store'
+import { useCallback, useEffect, useState } from 'react'
+import useDebounce from '../../lib/@jsl/src/js/react/hooks/use-debounce'
+import { fetchBillboard, fetchLikeBillboardPost } from '@/core/api-service'
+import { message } from 'antd'
+
+export const useList = () => {
+	const useSideSelectedKeys = useStore(e => e.useSideSelectedKeys)
+	useSideSelectedKeys('billboard')
+	const [data, setData] = useState({ content: [], total: 0 })
+	const [search, setSearch] = useState({
+		size: 10,
+		number: 1,
+		status: undefined,
+		sort: undefined,
+		order: undefined,
+		keyword: '',
+	})
+	const [loading, setLoading] = useState(true)
+
+	const onChangeSearch = (key, value) => {
+		setSearch(e => ({ ...e, number: 1, [key]: value }))
+	}
+
+	const onDebounceChangeSearch = useDebounce((key, value) => {
+		setSearch(e => ({ ...e, number: 1, [key]: value }))
+	}, 1000)
+
+	const onChangeTable = (pagination, filters, sorter) => {
+		if (
+			pagination.current !== search.number ||
+			pagination.pageSize !== search.size
+		) {
+			setSearch(e => ({
+				...e,
+				number: pagination.current,
+				size: pagination.pageSize,
+			}))
+			return
+		}
+		if (sorter.field !== search.sort || sorter.order !== search.order) {
+			setSearch(e => ({
+				...e,
+				number: 1,
+				sort: sorter.field,
+				order: sorter.order,
+			}))
+		}
+	}
+
+	const getList = useCallback(async () => {
+		setLoading(true)
+		const res = await fetchBillboard({
+			...search,
+			number: search.number - 1,
+		})
+		setLoading(false)
+		if (res.success) {
+			setData({ total: res.data.total, content: res.data.content })
+			return
+		}
+		setData({ total: 0, content: [] })
+		console.log('取得公佈欄列表完成')
+	}, [search])
+
+	const onLike = useCallback(
+		async (id, key = 'like', value = true) => {
+			const { success, message: resMessage } = await fetchLikeBillboardPost({
+				id,
+				[key]: value,
+			})
+			if (success) {
+				message.success(resMessage)
+				getList()
+			}
+		},
+		[getList],
+	)
+
+	useEffect(getList, [search])
+
+	return {
+		loading,
+		data,
+		search,
+		getList,
+		onChangeSearch,
+		onDebounceChangeSearch,
+		onChangeTable,
+		onLike,
+	}
+}
