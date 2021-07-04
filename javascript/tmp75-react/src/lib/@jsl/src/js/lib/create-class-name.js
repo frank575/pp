532e582ee349e2e25a666558d05bb8ac1e744387
吳俊緯 <wcw850710@gmail.com>
2021-07-05 01:36:12 +0800

    refactor(jsl/lib/create-class-name) v2 {author: frank575} JSDOC 類型修正

--- javascript/tmp75-react/src/lib/@jsl/src/js/lib/create-class-name.js
+++ javascript/tmp75-react/src/lib/@jsl/src/js/lib/create-class-name.js
@@ -1,11 +1,11 @@
 /// 似 Vue 的 class obj 方式寫 className
-/// author frank575
-/// v1 [broken] 改名為 createClassName
-/// v0
+/// v2 {author: frank575} JSDOC 類型修正
+/// v1 {author: frank575} [broken] 改名為 createClassName
+/// v0 {author: frank575}
 
 /**
  * @param {Object.<string, boolean>} obj
- * @param {...string} classNames
+ * @param {string[]} classNames
  * @return {string} 轉換後的className
  */
 export const createClassName = (obj, ...classNames) => {
