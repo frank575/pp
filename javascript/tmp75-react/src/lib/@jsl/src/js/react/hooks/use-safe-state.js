e493f50c6a8db15ea729913e9eef32f9ae197f22
吳俊緯 <wcw850710@gmail.com>
2021-07-05 10:07:00 +0800

    refactor(jsl/react/hooks/use-safe-state) v1 {author: frank575} parameter改名_state->initialState

--- javascript/tmp75-react/src/lib/@jsl/src/js/react/hooks/use-safe-state.js
+++ javascript/tmp75-react/src/lib/@jsl/src/js/react/hooks/use-safe-state.js
@@ -1,16 +1,17 @@
 /// 防止內存洩漏的 useState
+/// v1 {author: frank575} parameter改名_state->initialState
 /// v0 {author: frank575}
 
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 /**
  * @template T
- * @param {T} _state
+ * @param {T} initialState
  * @returns [T, function(T | function(T): *): void]
  */
-export const useSafeState = _state => {
+export const useSafeState = initialState => {
 	const _isMounted = useRef(true)
-	const [state, setState] = useState(_state)
+	const [state, setState] = useState(initialState)
 	const updateState = useCallback(update => {
 		if (_isMounted.current) {
 			setState(update)
