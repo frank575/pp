e33795dddd242533fc4ddaf57347bb7095a5ea5e
吳俊緯 <wcw850710@gmail.com>
2021-07-06 17:53:21 +0800

    build(tailwind.config) 改成extend

--- javascript/tmp75-react/tailwind.config.js
+++ javascript/tmp75-react/tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		maxWidth: {
-			'side-menu': '256px',
+		extend: {
+			maxWidth: {
+				'side-menu': '256px',
+			},
 		},
-		extend: {},
 	},
 	variants: {
 		extend: {},
