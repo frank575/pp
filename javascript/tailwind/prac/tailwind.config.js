c2872e1228aac29b37751246c69c721eb051fbd3
吳俊緯 <wcw850710@gmail.com>
2021-07-11 19:36:43 +0800

    feat(js/tailwind/prac) baha 首頁練習完成

--- javascript/tailwind/prac/tailwind.config.js
+++ javascript/tailwind/prac/tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        banner: '160px'
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      colors: {
+        baha: '#009cad'
+      }
+    },
   },
   variants: {
     extend: {},
