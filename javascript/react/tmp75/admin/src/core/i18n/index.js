6c793f7e3be52498a459a6a12dd7e34520b74608
吳俊緯 <wcw850710@gmail.com>
2021-07-14 09:17:40 +0800

    refactor(core/i18n) 新增 e-locale

--- javascript/react/tmp75/admin/src/core/i18n/index.js
+++ javascript/react/tmp75/admin/src/core/i18n/index.js
@@ -1,15 +1,16 @@
 import { createI18n } from '@/lib/@jsl/src/js/react/lib/i18n'
 import { zh_TW } from '@/core/i18n/zh_TW'
 import { zh_CN } from '@/core/i18n/zh_CN'
+import { ELocale } from '@/enums/e-locale'
 
 export const {
 	t,
 	Provider: I18nProvider,
 	inject: useI18n,
 } = createI18n({
-	locale: 'zh_TW',
+	locale: ELocale.zh_TW,
 	messages: {
-		zh_TW,
-		zh_CN,
+		[ELocale.zh_TW]: zh_TW,
+		[ELocale.zh_CN]: zh_CN,
 	},
 })
---
+++ javascript/react/tmp75/admin/src/enums/e-locale.js
@@ -0,0 +1,6 @@
+import { createEnum } from '@jsl'
+
+export const ELocale = createEnum({
+	zh_TW: 0,
+	zh_CN: 1,
+})
