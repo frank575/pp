98b0953c830ea1b7cf9e04c0fa1c658b31675cf1
吳俊緯 <wcw850710@gmail.com>
2021-07-14 17:36:45 +0800

    feat(core/not-found) 添加樣式

--- javascript/react/tmp75/admin/src/core/components/not-found.jsx
+++ javascript/react/tmp75/admin/src/core/components/not-found.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Button, Result } from 'antd'
+import { Link } from 'react-router-dom'
 
 export default () => {
-	return <div>404 Not Found!</div>
+	return (
+		<div className="flex items-center justify-center min-w-full min-h-screen bg-gray-200">
+			<div className="bg-white shadow-md rounded-md">
+				<Result
+					status="404"
+					title="404"
+					subTitle={'抱歉，該頁面不存在。'}
+					extra={
+						<Link to={'/'}>
+							<Button type="primary">回首頁</Button>
+						</Link>
+					}
+				/>
+			</div>
+		</div>
+	)
 }
