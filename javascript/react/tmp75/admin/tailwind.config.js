10013ad34f1cd49d10a8b7f37a6a6cb0fec52182
吳俊緯 <wcw850710@gmail.com>
2021-07-15 16:43:34 +0800

    build(tailwind.config) breakpoint 改成 max-width, colors 補上 color 設計

--- javascript/react/tmp75/admin/tailwind.config.js
+++ javascript/react/tmp75/admin/tailwind.config.js
@@ -3,15 +3,29 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	important: true,
 	theme: {
+		screens: {
+			'2xl': { max: '1535px' },
+			xl: { max: '1279px' },
+			lg: { max: '1023px' },
+			md: { max: '767px' },
+			sm: { max: '639px' },
+			xs: { max: '424px' },
+		},
 		extend: {
-			maxWidth: {
-				'side-menu': '256px',
+			colors: {
+				default: '#000000d9',
+				success: '#52c41a',
+				primary: '#1890ff',
+				warning: '#faad14',
+				danger: '#ff4d4f',
+				secondary: '#00000073',
+				disabled: '#00000040',
 			},
 			borderWidth: {
 				1: '1px',
 			},
-			screens: {
-				xs: '425px',
+			maxWidth: {
+				'side-menu': '256px',
 			},
 		},
 	},
