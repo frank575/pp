217aab3b8c03ed93e873f3c322add82db3800cd5
吳俊緯 <wcw850710@gmail.com>
2021-07-17 07:53:58 +0800

    build(vite.config) 環境配置方式調整

--- javascript/react/tmp75/admin/vite.config.js
+++ javascript/react/tmp75/admin/vite.config.js
@@ -1,60 +1,66 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import vitePluginImport from 'vite-plugin-babel-import'
 const path = require('path')
-require('./ci/index')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		reactRefresh(),
-		vitePluginImport([
-			{
-				libraryName: 'antd',
-				libraryDirectory: 'es',
-				style(name) {
-					return `antd/es/${name}/style`
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+	return defineConfig({
+		plugins: [
+			reactRefresh(),
+			vitePluginImport([
+				{
+					libraryName: 'antd',
+					libraryDirectory: 'es',
+					style(name) {
+						return `antd/es/${name}/style`
+					},
+				},
+				// {
+				// 	libraryName: 'antd-mobile',
+				// 	libraryDirectory: 'es',
+				// 	style(name) {
+				// 		return `antd-mobile/es/${name}/style`
+				// 	},
+				// },
+			]),
+		],
+		css: {
+			preprocessorOptions: {
+				less: {
+					javascriptEnabled: true,
 				},
 			},
-			// {
-			// 	libraryName: 'antd-mobile',
-			// 	libraryDirectory: 'es',
-			// 	style(name) {
-			// 		return `antd-mobile/es/${name}/style`
-			// 	},
-			// },
-		]),
-	],
-	css: {
-		preprocessorOptions: {
-			less: {
-				javascriptEnabled: true,
+		},
+		// esbuild: {
+		// 	jsxInject: `import React from 'react'`,
+		// },
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src'),
+				'@jsl': path.resolve(__dirname, 'src/core/jsl/src/js/lib'),
+				'@jsl-hooks': path.resolve(
+					__dirname,
+					'src/core/jsl/src/js/react/hooks',
+				),
+				'@i18n': path.resolve(__dirname, 'src/core/i18n'),
 			},
 		},
-	},
-	// esbuild: {
-	// 	jsxInject: `import React from 'react'`,
-	// },
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-			'@jsl': path.resolve(__dirname, 'src/core/jsl/src/js/lib'),
-			'@jsl-hooks': path.resolve(__dirname, 'src/core/jsl/src/js/react/hooks'),
-			'@i18n': path.resolve(__dirname, 'src/core/i18n'),
-		},
-	},
-	server: {
-		port: process.env.VITE_PORT,
-		proxy: {
-			[process.env.VITE_API_BASE_URL]: {
-				target: process.env.VITE_API_URL,
-				changeOrigin: true,
+		server: {
+			port: process.env.VITE_PORT,
+			proxy: {
+				[process.env.VITE_API_BASE_URL]: {
+					target: process.env.VITE_API_URL,
+					changeOrigin: true,
+				},
+				// [process.env.VITE_WS_BASE_URL]: {
+				// 	target: process.env.VITE_API_URL,
+				// 	changeOrigin: true,
+				// 	// rewrite: path => path.replace(/^\/ws/, '')
+				// }
 			},
-			// [process.env.VITE_WS_BASE_URL]: {
-			// 	target: process.env.VITE_API_URL,
-			// 	changeOrigin: true,
-			// 	// rewrite: path => path.replace(/^\/ws/, '')
-			// }
 		},
-	},
-})
+	})
+}
