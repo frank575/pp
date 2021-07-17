3caedb7ff28c1c33deaf82e5459e969656832f81
吳俊緯 <wcw850710@gmail.com>
2021-07-17 14:52:15 +0800

    feat(js/vite/prac) add glob-import

--- javascript/vite/prac/main.js
+++ javascript/vite/prac/main.js
@@ -12,10 +12,10 @@ worker.onmessage = (e) => {
   console.log(`onmessage: ${e.data}`);
 };
 
-function render() {
+export function render() {
   document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation...</a>
 `;
 }
 
@@ -24,7 +24,35 @@ render();
 // hmr-api 用來實現熱更新，原理是用socket通知
 // 當你調用hot.accept將會關閉vite自動重整的功能
 if (import.meta.hot) {
+  // newModule 會取出新的模塊，該檔僅導出 render 故 newModule 內只有 render 這個模塊
   import.meta.hot.accept((newModule) => {
+    worker.terminate()
+    // 重新render達到熱更新
     newModule.render();
   });
 }
+
+// glob-import 批量導入功能，這裡導入 glob 目錄下的所有模塊
+// 為vite功能而非es-module，由第三方庫fast-glob做的
+// 編譯結果會把檔案取出使用異步加載導入
+const globModules = import.meta.glob("./glob/*[a-c].(js|json)");
+
+console.log('------------')
+console.log('globModules')
+console.log(globModules);
+
+Object.entries(globModules).forEach(([k, v]) => {
+  v().then((m) => {
+    console.log(k)
+    console.log(m.default)
+  });
+});
+
+
+// globEager 會編譯成靜態資料在代碼中(同步)
+const globEagerModules = import.meta.globEager("./glob/d.json");
+
+console.log('------------')
+console.log('globEagerModules')
+console.log(globEagerModules)
+console.log(globEagerModules['./glob/d.json'].default)
