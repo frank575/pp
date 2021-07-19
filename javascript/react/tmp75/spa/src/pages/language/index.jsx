fbf9e2d13d76c391d0e7c91061c55d8a32ee3330
吳俊緯 <wcw850710@gmail.com>
2021-07-19 23:54:21 +0800

    refactor(pages/language) 新增 @i18n.t 範例

--- javascript/react/tmp75/spa/src/pages/language/index.jsx
+++ javascript/react/tmp75/spa/src/pages/language/index.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { AppContent } from '@/components/app/content'
 import { AppTitle } from '@/components/app/title'
 import { TranslationOutlined } from '@ant-design/icons'
-import { Form, Select } from 'antd'
+import { Form, message, Select } from 'antd'
 import { ELocale } from '@/enums/e-locale'
-import { useI18n } from '@i18n'
+import { t, useI18n } from '@i18n'
+
+const callSuccessMessage = () => {
+	// 也可以從 @i18n 內取出 t 方法進行翻譯，支援任意位置調用
+	// 如果要刷新頁面請使用 useI18n 取出 t
+	message.success(t('changeMessageSuccess'))
+}
 
 export default () => {
 	const { locale, changeLocale, t } = useI18n(e => e)
-	const onChangeLocale = _locale => changeLocale(_locale)
+	const onChangeLocale = _locale => {
+		changeLocale(_locale)
+		callSuccessMessage()
+	}
 
 	return (
 		<AppContent>
