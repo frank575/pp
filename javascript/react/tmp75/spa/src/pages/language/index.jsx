ada26a18afb6f8cc67c7e2c35ecdb6c5407b31d0
吳俊緯 <wcw850710@gmail.com>
2021-07-19 23:43:51 +0800

    feat(pages/language) 完善多國語系示範頁面

--- javascript/react/tmp75/spa/src/pages/language/index.jsx
+++ javascript/react/tmp75/spa/src/pages/language/index.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import { AppContent } from '@/components/app/content'
 import { AppTitle } from '@/components/app/title'
 import { TranslationOutlined } from '@ant-design/icons'
+import { Form, Select } from 'antd'
+import { ELocale } from '@/enums/e-locale'
+import { useI18n } from '@i18n'
 
 export default () => {
+	const { locale, changeLocale, t } = useI18n(e => e)
+	const onChangeLocale = _locale => changeLocale(_locale)
+
 	return (
 		<AppContent>
 			<AppTitle title={'多國語系'} icon={TranslationOutlined} just />
+			<Form.Item label="當前語系" className="mb-2">
+				<Select value={locale} onChange={onChangeLocale}>
+					{ELocale.map(([v, t]) => (
+						<Select.Option key={v} value={v}>
+							{t}
+						</Select.Option>
+					))}
+				</Select>
+			</Form.Item>
+			<Form.Item label="示範單字" className="mb-0">
+				{t('handsomeX', ['Frank'])}
+			</Form.Item>
 		</AppContent>
 	)
 }
