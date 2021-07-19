a4c03e6071252cfdb4e5dd93202702f838344907
吳俊緯 <wcw850710@gmail.com>
2021-07-20 00:13:10 +0800

    feat(jsl/lib/create-enum) v2 {author: frank575} [broken] map 返回改成 v, k，並新增 reduce 方法

--- javascript/react/tmp75/spa/src/core/jsl/src/js/lib/create-enum.js
+++ javascript/react/tmp75/spa/src/core/jsl/src/js/lib/create-enum.js
@@ -1,21 +1,29 @@
 /// enum 實用的狀態轉換器
+/// v2 {author: frank575} [broken] map 返回改成 v, k，並新增 reduce 方法
 /// v1 {author: frank575} [broken] Enum 改名為 createEnum
 /// v0 {author: frank575}
 
 /**
  * @template T
  * @param {T} obj
- * @return {T & {t(val: any): string, key(val: any): string, keys: string[], map: function(callback?: function(v: [any, any], k: string)): any[]}}
+ * @return {T & {t(val: *): string, key(val: *): string, keys: string[], map: function(callback?: function(*, string): *): *[], reduce: function(callback: function(*, *, string): *, initialValue: *): *}}
  */
 export const createEnum = obj => {
 	const translation = {}
 	const reverseEnum = {}
 	const $enum = {}
+	const keys = []
 	const t = val => translation[val]
 	const key = val => reverseEnum[val]
 	const map = callback =>
-		callback && keys.map(k => callback([$enum[k], t($enum[k])], k))
-	const keys = []
+		callback ? keys.map(k => callback($enum[k], k)) : keys
+	const reduce = (callback, initialValue) => {
+		let previous = initialValue
+		keys.map(k => {
+			previous = callback(previous, $enum[k], k)
+		})
+		return previous
+	}
 
 	function addEnum(key, val) {
 		if (Array.isArray(val)) {
@@ -34,5 +42,5 @@ export const createEnum = obj => {
 		addEnum(k, obj[k])
 	}
 
-	return { ...$enum, t, key, keys, map }
+	return { ...$enum, t, key, keys, map, reduce }
 }
