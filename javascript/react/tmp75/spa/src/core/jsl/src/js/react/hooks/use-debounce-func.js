a1979e67a19729a0d79adfec4ff24143fdb97abc
吳俊緯 <wcw850710@gmail.com>
2021-07-21 09:25:25 +0800

    feat(jsl/react/hooks/use-debounce-func) v1 {author: frank575} [broken] 新增 clearTimeout 方法，並將返回改成 array

--- javascript/react/tmp75/spa/src/core/jsl/src/js/react/hooks/use-debounce-func.js
+++ javascript/react/tmp75/spa/src/core/jsl/src/js/react/hooks/use-debounce-func.js
@@ -1,25 +1,33 @@
 /// 防抖方法鉤子
+/// v1 {author: frank575} [broken] 新增 clearTimeout 方法，並將返回改成 array
 /// v0 {author: frank575}
 
 import { useCallback, useRef } from 'react'
 
 /**
- * @template T
- * @param {T} func
+ * @param {function(...[*]): void} func
  * @param [delay=500]
- * @returns {T}
+ * @return {[function(...[*]): void, function(): void]}
  */
 export const useDebounceFunc = (func, delay = 500) => {
 	const timeout = useRef(null)
 
-	return useCallback(
+	const _clearTimeout = useCallback(() => {
+		if (timeout.current != null) {
+			clearTimeout(timeout.current)
+		}
+	}, [])
+
+	const debounceFunc = useCallback(
 		(...args) => {
-			if (timeout.current) clearTimeout(timeout.current)
+			_clearTimeout()
 			timeout.current = setTimeout(() => {
 				func(...args)
 				timeout.current = null
 			}, delay)
 		},
-		[func, delay],
+		[func, delay, _clearTimeout],
 	)
+
+	return [debounceFunc, _clearTimeout]
 }
