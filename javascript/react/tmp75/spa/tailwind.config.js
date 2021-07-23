b96c5fb51325fdcc945fe04f757b204d7900cafe
吳俊緯 <wcw850710@gmail.com>
2021-07-23 15:15:32 +0800

    refactor(tailwind.config) screens max width 全+1

--- javascript/react/tmp75/spa/tailwind.config.js
+++ javascript/react/tmp75/spa/tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
 	important: true,
 	theme: {
 		screens: {
-			'2xl': { max: '1535px' },
-			xl: { max: '1279px' },
-			lg: { max: '1023px' },
-			md: { max: '767px' },
-			sm: { max: '639px' },
-			xs: { max: '424px' },
+			'2xl': { max: '1536px' },
+			xl: { max: '1280px' },
+			lg: { max: '1024px' },
+			md: { max: '768px' },
+			sm: { max: '640px' },
+			xs: { max: '425px' },
 		},
 		extend: {
 			colors: {
