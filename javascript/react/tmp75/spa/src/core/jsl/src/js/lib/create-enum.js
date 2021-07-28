a4fa21779adb83dd2695ed32ef7787be1ae53260
吳俊緯 <wcw850710@gmail.com>
2021-07-28 13:11:09 +0800

    feat(jsl/create-enum) v3 {author: frank575} map, reduce 返回 index

--- javascript/react/tmp75/spa/src/core/jsl/src/js/lib/create-enum.js
+++ javascript/react/tmp75/spa/src/core/jsl/src/js/lib/create-enum.js
@@ -1,4 +1,5 @@
 /// enum 實用的狀態轉換器
+/// v3 {author: frank575} map, reduce 返回 index
 /// v2 {author: frank575} [broken] map 返回改成 v, k，並新增 reduce 方法
 /// v1 {author: frank575} [broken] Enum 改名為 createEnum
 /// v0 {author: frank575}
@@ -16,11 +17,11 @@ export const createEnum = obj => {
 	const t = val => translation[val]
 	const key = val => reverseEnum[val]
 	const map = callback =>
-		callback ? keys.map(k => callback($enum[k], k)) : keys
+		callback ? keys.map((k, i) => callback($enum[k], k, i)) : keys
 	const reduce = (callback, initialValue) => {
 		let previous = initialValue
-		keys.map(k => {
-			previous = callback(previous, $enum[k], k)
+		keys.map((k, i) => {
+			previous = callback(previous, $enum[k], k, i)
 		})
 		return previous
 	}
