67bac2f2e0403f147a782dd8ab1fad87dcf3a8ec
吳俊緯 <wcw850710@gmail.com>
2021-07-29 10:16:04 +0800

    fix(jsl/react/hooks/use-cache-state) v1 {author: frank575} 修正初始化沒寫入緩存

--- javascript/react/tmp75/spa/src/core/jsl/src/js/react/hooks/use-cache-state.js
+++ javascript/react/tmp75/spa/src/core/jsl/src/js/react/hooks/use-cache-state.js
@@ -1,7 +1,8 @@
 /// 自動緩存到內存的useState
+/// v1 {author: frank575} 修正初始化沒寫入緩存
 /// v0 {author: frank575}
 
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSafeState } from './use-safe-state'
 
 const CACHE_STATE = {}
@@ -14,6 +15,7 @@ const CACHE_STATE = {}
  */
 export const useCacheState = (symbol, initialState) => {
 	const [state, setState] = useSafeState(CACHE_STATE[symbol] ?? initialState)
+
 	const updateState = useCallback(cb => {
 		setState(e => {
 			let _state
@@ -26,5 +28,12 @@ export const useCacheState = (symbol, initialState) => {
 			return _state
 		})
 	}, [])
+
+	useEffect(() => {
+		if (CACHE_STATE[symbol] == null) {
+			CACHE_STATE[symbol] = initialState
+		}
+	}, [])
+
 	return [state, updateState]
 }
