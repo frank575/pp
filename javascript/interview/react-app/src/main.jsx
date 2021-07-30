1aa798c9083dc72dfa927436806554532e2d63ec
吳俊緯 <wcw850710@gmail.com>
2021-07-30 10:34:45 +0800

    refactor(js/interview/react-app) BrowserRouter > HashRouter

--- javascript/interview/react-app/src/main.jsx
+++ javascript/interview/react-app/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './style/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
