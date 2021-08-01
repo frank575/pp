56c7f4f7f5471ddf774bb10c6b18303e5709484f
吳俊緯 <wcw850710@gmail.com>
2021-08-01 23:27:09 +0800

    build(js/react/tmp75/ssr/package.json) 新增依賴

---
+++ javascript/react/tmp75/ssr/tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	important: true,
+	theme: {
+		screens: {
+			'2xl': { max: '1536px' },
+			xl: { max: '1280px' },
+			lg: { max: '1024px' },
+			md: { max: '768px' },
+			sm: { max: '640px' },
+			xs: { max: '425px' },
+		},
+		extend: {
+			colors: {
+				default: '#000000d9',
+				success: '#52c41a',
+				primary: '#1890ff',
+				warning: '#faad14',
+				danger: '#ff4d4f',
+				secondary: '#00000073',
+				disabled: '#00000040',
+			},
+			borderWidth: {
+				1: '1px',
+			},
+			maxWidth: {
+				'side-menu': '256px',
+				// 'screen-2xl': '1536px',
+				// 'screen-xl': '1280px',
+				// 'screen-lg': '1024px',
+				'screen-md': '768px',
+				// 'screen-sm': '640px',
+				// 'screen-xs': '425px',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
--- javascript/react/tmp75/ssr/vite.config.js
+++ javascript/react/tmp75/ssr/vite.config.js
@@ -1,6 +1,9 @@
+import { defineConfig, loadEnv } from 'vite'
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import ssr from "vite-plugin-ssr/plugin";
+const path = require('path')
 
-export default {
-  plugins: [reactRefresh(), ssr()],
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({plugins: [reactRefresh(), ssr()],})
 };
