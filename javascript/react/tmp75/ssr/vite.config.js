2aa31cb61165e5353d710f27bf863b18ce4a791f
吳俊緯 <wcw850710@gmail.com>
2021-08-02 13:51:51 +0800

    feat(js/react/tmp75/ssr) init next + tailwindcss + prettier

--- javascript/react/tmp75/ssr/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig, loadEnv } from 'vite'
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import ssr from "vite-plugin-ssr/plugin";
-const path = require('path')
-
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-  return defineConfig({plugins: [reactRefresh(), ssr()],})
-};
