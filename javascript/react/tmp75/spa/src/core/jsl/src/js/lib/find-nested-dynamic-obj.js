a52e35353e845b24c4713f60f5038524a80faa66
吳俊緯 <wcw850710@gmail.com>
2021-08-20 11:01:21 +0800

    fix: find-nested-dynamic-obj.js v1 {author: frank575} 重寫邏輯(原本懶得寫用他人的)，修正 cannot read property 'split' of undefined 的錯誤

--- javascript/react/tmp75/spa/src/core/jsl/src/js/lib/find-nested-dynamic-obj.js
+++ javascript/react/tmp75/spa/src/core/jsl/src/js/lib/find-nested-dynamic-obj.js
@@ -1,4 +1,5 @@
 /// 迴圈查詢嵌套物件的值
+/// v1 {author: frank575} 重寫邏輯(原本懶得寫用他人的)，修正 cannot read property 'split' of undefined 的錯誤
 /// v0 {author: frank575}
 
 /**
@@ -8,23 +9,34 @@
  * @return {*} 找出的值
  */
 export const findNestedDynamicObj = (obj, key) => {
-	let result = obj[key] || key
-	if (obj[key] != null && typeof obj[key] !== 'object') return result
+	if (typeof obj !== 'object') {
+		console.warn(`[findNestedDynamicObj::warn] obj 請傳 {}`)
+		return key
+	}
+	const _key = typeof key === 'string' ? key : String(key)
+	const ks = _key.split('.')
+	let el = obj[_key] || key
 
-	const sptKeys = key.split('.')
-	if (sptKeys.length < 2) return result
-
-	let i = 0,
-		el = obj[sptKeys[i]]
-
-	while (el != null) {
-		el = el[sptKeys[++i]]
-		if (typeof el === 'object') {
-			continue
-		}
-		result = el
-		break
+	if (typeof el !== 'object' && ks.length < 2) {
+		return el
 	}
 
-	return result
+	el = obj[ks[0]]
+	for (let i = 1; i < ks.length; i++) {
+		if (el == null) {
+			break
+		}
+
+		const k = ks[i]
+		el = el[k] || key
+
+		if (typeof el !== 'object') {
+			if (i !== ks.length - 1) {
+				el = key
+			}
+			break
+		}
+	}
+
+	return el || key
 }
