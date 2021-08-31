31a0e0ee0bef2ea198dfd1f2400f626416c58b7a
吳俊緯 <wcw850710@gmail.com>
2021-08-31 12:02:00 +0800

    feat: 新增大小隨機判斷

--- javascript/l8/upgrade/q1/src/pages/account/login/captcha/click-num.jsx
+++ javascript/l8/upgrade/q1/src/pages/account/login/captcha/click-num.jsx
@@ -1,6 +1,6 @@
 import { useVisible } from '@/hooks/use-visible'
 import { ModalWrap } from '@/pages/account/login/captcha/modal-wrap'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export const ClickNumCaptcha = ({
 	visible: propVisible,
@@ -8,11 +8,17 @@ export const ClickNumCaptcha = ({
 	onLogin,
 }) => {
 	const [visible, setVisible] = useVisible(propVisible, onChangeVisible)
-	const [isSmall, setIsSmall] = useState(false)
+	const [isSmall, setIsSmall] = useState(true)
 	const title = useMemo(() => {
 		const [b, a] = isSmall ? '大小' : '小大'
 		return `請由數字最${b}點到最${a}`
 	}, [isSmall])
+
+	useEffect(() => {
+		if (visible) {
+			setIsSmall(Math.random() < 0.5)
+		}
+	}, [visible])
 
 	return (
 		<ModalWrap visible={visible} setVisible={setVisible}>
