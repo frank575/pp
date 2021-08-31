4403aabc6e49e1dcf34f6ff258ac8ef3fe27d669
吳俊緯 <wcw850710@gmail.com>
2021-08-31 09:18:46 +0800

    refactor: 初始化圖形及數字點擊布局及標題

--- javascript/l8/upgrade/q1/src/pages/account/login/captcha/click-num.jsx
+++ javascript/l8/upgrade/q1/src/pages/account/login/captcha/click-num.jsx
@@ -1,5 +1,6 @@
 import { useVisible } from '@/hooks/use-visible'
 import { ModalWrap } from '@/pages/account/login/captcha/modal-wrap'
+import { useMemo, useState } from 'react'
 
 export const ClickNumCaptcha = ({
 	visible: propVisible,
@@ -7,10 +8,18 @@ export const ClickNumCaptcha = ({
 	onLogin,
 }) => {
 	const [visible, setVisible] = useVisible(propVisible, onChangeVisible)
+	const [isSmall, setIsSmall] = useState(false)
+	const title = useMemo(() => {
+		const [b, a] = isSmall ? '大小' : '小大'
+		return `請由數字最${b}點到最${a}`
+	}, [isSmall])
 
 	return (
 		<ModalWrap visible={visible} setVisible={setVisible}>
-			PatternCaptcha
+			<div className="relative text-center">
+				<div className="bg-white" style={{ width: 450, height: 300 }}></div>
+				<div className="text-white mt-5">{title}</div>
+			</div>
 		</ModalWrap>
 	)
 }
--- javascript/l8/upgrade/q1/src/pages/account/login/captcha/pattern.jsx
+++ javascript/l8/upgrade/q1/src/pages/account/login/captcha/pattern.jsx
@@ -10,7 +10,10 @@ export const PatternCaptcha = ({
 
 	return (
 		<ModalWrap visible={visible} setVisible={setVisible}>
-			PatternCaptcha
+			<div className="relative text-center">
+				<div className="bg-white" style={{ width: 360, height: 360 }}></div>
+				<div className="text-white mt-12">請照順序滑動解鎖</div>
+			</div>
 		</ModalWrap>
 	)
 }
