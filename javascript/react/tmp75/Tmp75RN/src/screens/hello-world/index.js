cfb12380391b541a69b3a6a9d7548298f9dc2a10
吳俊緯 <wcw850710@gmail.com>
2021-09-10 07:32:20 +0800

    feat: 初始化 app

---
+++ javascript/react/tmp75/Tmp75RN/src/screens/hello-world/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+
+export const HelloWorld = () => {
+	return <SafeAreaView style={tw`flex-1`}>hello world!</SafeAreaView>
+}
