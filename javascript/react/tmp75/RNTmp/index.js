7f179902a9364341b4a756085bda7ec5cc8e8187
吳俊緯 <wcw850710@gmail.com>
2021-09-10 13:28:45 +0800

    feat: 新增 navigation drawer

--- javascript/react/tmp75/RNTmp/index.js
+++ javascript/react/tmp75/RNTmp/index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from '@/core/app'
 import { name as appName } from './app.json'
--- javascript/react/tmp75/RNTmp/src/core/app.jsx
+++ javascript/react/tmp75/RNTmp/src/core/app.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { HelloReactNative } from '@/screens/hello-react-native'
 import { Home } from '@/screens/home'
 
+const Drawer = createDrawerNavigator()
 const Stack = createNativeStackNavigator()
 
 export const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="HelloReactNative" component={HelloReactNative} />
-				<Stack.Screen name="Home" component={Home} />
-			</Stack.Navigator>
+			<Drawer.Navigator initialRouteName="HelloReactNative">
+				<Drawer.Screen name="HelloReactNative" component={HelloReactNative} />
+				<Drawer.Screen name="Home" component={Home} />
+			</Drawer.Navigator>
 		</NavigationContainer>
 	)
 }
+// <Stack.Navigator>
+// 	<Stack.Screen name="HelloReactNative" component={HelloReactNative} />
+// 	<Stack.Screen name="Home" component={Home} />
+// </Stack.Navigator>
 
 export default App
