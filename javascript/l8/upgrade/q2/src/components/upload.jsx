93ce0f58fc0831b82323e52310521be4681b110e
吳俊緯 <wcw850710@gmail.com>
2021-09-12 23:34:35 +0800

    新增: 上傳圖片組件新增 loading prop

--- javascript/l8/upgrade/q2/src/components/upload.jsx
+++ javascript/l8/upgrade/q2/src/components/upload.jsx
@@ -2,7 +2,11 @@ import { Button } from '@/components/button'
 import { useRef } from 'react'
 import { createClassName } from '@jsl'
 
-export const Upload = ({ className, onChange: onPropChange }) => {
+export const Upload = ({
+	className,
+	onChange: onPropChange,
+	loading = false,
+}) => {
 	const fileRef = useRef(null)
 	const onUpload = () => fileRef.current?.click()
 	const onChange = ev => onPropChange?.(ev.target.files[0])
@@ -13,7 +17,9 @@ export const Upload = ({ className, onChange: onPropChange }) => {
 				[className]: className != null,
 			})}
 		>
-			<Button onClick={onUpload}>上傳</Button>
+			<Button loading={loading} onClick={onUpload}>
+				上傳
+			</Button>
 			<input className="hidden" ref={fileRef} type="file" onChange={onChange} />
 		</div>
 	)
