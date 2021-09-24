68f5362d3ba161d389d1616c68e8f63d97b8660e
吳俊緯 <wcw850710@gmail.com>
2021-09-24 10:12:13 +0800

    新增：@jsl type 定義完工，並且移除 i18n 的 storage 配置項

--- javascript/react/tmp75/@jsl/react/common/create-i18n.js
+++ javascript/react/tmp75/@jsl/react/common/create-i18n.js
@@ -1,12 +1,11 @@
 /// create-i18n 可以在任意地方翻譯(t)的國際化
+/// v2 {author: frank575} 移除 storage 配置項
 /// v1 {author: frank575} useHook 提供 t 函數
 /// v0 {author: frank575}
 
 import { useCallback, useEffect, useState } from 'react'
 import { createProvider } from './create-provider'
 import { findNestedDynamicObj } from '../../common/common/find-nested-dynamic-obj'
-import { useLocalStorageState } from '../web/storage/use-local-storage-state'
-import { useSessionStorageState } from '../web/storage/use-session-storage-state'
 
 export { createI18n }
 
@@ -22,27 +21,10 @@ const translate = (messages, nestedKey, replaceArrayStr = []) => {
 }
 
 const service =
-	({
-		messages,
-		locale: initialLocale,
-		_setLocale,
-		storageName,
-		storageKey,
-		t: _t,
-	}) =>
+	({ messages, locale: initialLocale, _setLocale, t: _t }) =>
 	() => {
-		const [locale, changeLocale] =
-			storageName === 'sessionStorage'
-				? useSessionStorageState(storageKey, initialLocale)
-				: storageName === 'localStorage'
-				? useLocalStorageState(storageKey, initialLocale)
-				: useState(initialLocale)
+		const [locale, changeLocale] = useState(initialLocale)
 
-		/**
-		 * @param {string} nestedKey 標題
-		 * @param [replaceArrayStr=[]: string[]] 覆蓋的索引內容
-		 * @return {string} 翻譯結果
-		 */
 		const t = useCallback(
 			(nestedKey, replaceArrayStr = []) =>
 				translate(messages[locale], nestedKey, replaceArrayStr),
@@ -60,34 +42,15 @@ const service =
 		}
 	}
 
-/**
- * @typedef {Object.<string, string | Message>} Message
- */
-
-/**
- * @template T
- * @param options 選項
- * @param {*} options.locale locale
- * @param {Message} options.messages 翻譯的文字們
- * @param [options.storageName=null: 'localStorage' | 'sessionStorage' | null] 是否要存到緩存裏，有名稱表示要存
- * @param {string} options.storageKey 存到緩存的 key
- */
-function createI18n({ locale, messages, storageName, storageKey } = {}) {
+function createI18n({ locale, messages } = {}) {
 	let _locale = locale
 	const _setLocale = locale => (_locale = locale)
 
-	/**
-	 * @param {string} nestedKey 標題
-	 * @param [replaceArrayStr=[]: string[]] 覆蓋的索引內容
-	 * @return {string} 翻譯結果
-	 */
 	const t = (nestedKey, replaceArrayStr = []) =>
 		translate(messages[_locale], nestedKey, replaceArrayStr)
 
 	return {
 		t,
-		...createProvider(
-			service({ locale, messages, _setLocale, storageName, storageKey }),
-		),
+		...createProvider(service({ locale, messages, _setLocale })),
 	}
 }
--- javascript/react/tmp75/@jsl/type.d.ts
+++ javascript/react/tmp75/@jsl/type.d.ts
@@ -1,238 +1,277 @@
-type Prev = [
-  never,
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  ...0[]
-]
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ...0[]]
 
-type Next = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  ...10[]
-]
+type Next = [1, 2, 3, 4, 5, 6, 7, 8, 9, ...10[]]
 
 type Join<K, P> = K extends string | number
-  ? P extends string | number
-    ? `${K}${'' extends P ? '' : '.'}${P}`
-    : never
-  : never
+	? P extends string | number
+		? `${K}${'' extends P ? '' : '.'}${P}`
+		: never
+	: never
 
-type Paths<T, D extends number = 5> = [D] extends [never]
-  ? never
-  : T extends object
-    ? {
-      [K in keyof T]-?: K extends string | number
-        ? `${K}` | (Paths<T[K], Prev[D]> extends infer R ? Join<K, R> : never)
-        : never
-    }[keyof T]
-    : ''
+type Paths<T, D extends number = 10> = [D] extends [never]
+	? never
+	: T extends object
+	? {
+			[K in keyof T]-?: K extends string | number
+				? `${K}` | (Paths<T[K], Prev[D]> extends infer R ? Join<K, R> : never)
+				: never
+	  }[keyof T]
+	: ''
 
-type Leaves<T, D extends number = 5> = [D] extends [never]
-  ? never
-  : T extends object
-    ? { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T]
-    : ''
+type Leaves<T, D extends number = 10> = [D] extends [never]
+	? never
+	: T extends object
+	? { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T]
+	: ''
 
-type Cons<H, T> = T extends readonly any[] ?
-  ((h: H, ...t: T) => void) extends ((...r: infer R) => void) ? R : never
-  : never;
+type Cons<H, T> = T extends readonly any[]
+	? ((h: H, ...t: T) => void) extends (...r: infer R) => void
+		? R
+		: never
+	: never
 
-type PathArray<T, D extends number = 5> = [D] extends [never] ? never : T extends object ?
-  {
-    [K in keyof T]-?: [K] | (Paths<T[K], Prev[D]> extends infer P ?
-    P extends [] ? never : Cons<K, P> : never
-    )
-  }[keyof T]
-  : [];
+type PathArray<T, D extends number = 10> = [D] extends [never]
+	? never
+	: T extends object
+	? {
+			[K in keyof T]-?:
+				| [K]
+				| (Paths<T[K], Prev[D]> extends infer P
+						? P extends []
+							? never
+							: Cons<K, P>
+						: never)
+	  }[keyof T]
+	: []
 
-type LeaveArray<T, D extends number = 5> = [D] extends [never] ? never : T extends object ?
-  { [K in keyof T]-?: Cons<K, Leaves<T[K], Prev[D]>> }[keyof T]
-  : [];
+type LeaveArray<T, D extends number = 10> = [D] extends [never]
+	? never
+	: T extends object
+	? { [K in keyof T]-?: Cons<K, Leaves<T[K], Prev[D]>> }[keyof T]
+	: []
 
 type Split<S extends string, D extends string> = string extends S
-  ? string[]
-  : S extends ''
-    ? []
-    : S extends `${infer T}${D}${infer U}`
-      ? [T, ...Split<U, D>]
-      : [S]
+	? string[]
+	: S extends ''
+	? []
+	: S extends `${infer T}${D}${infer U}`
+	? [T, ...Split<U, D>]
+	: [S]
 
-type DeepValue<T extends object, KS extends (string | number)[], KSL extends number = KS['length'], D extends number = 0> = KSL extends 0
-  ? undefined
-  : KSL extends Next[D]
-    ? KS[D] extends keyof T
-      ? T[KS[D]]
-      : string | number
-    : DeepValue<T, KS>
+type DeepValue<
+	T extends object,
+	KS extends (string | number)[],
+	KSL extends number = KS['length'],
+	D extends number = 0,
+> = KSL extends 0
+	? undefined
+	: KSL extends Next[D]
+	? KS[D] extends keyof T
+		? T[KS[D]]
+		: string | number
+	: DeepValue<T, KS>
 
-type ValueOf<T> = T[keyof T];
+type ValueOf<T> = T[keyof T]
 
-declare module "@jsl" {
-  import {Moment} from "moment";
-  import {Dispatch} from "react";
+declare module '@jsl' {
+	import { Moment } from 'moment'
 
+	export function findNestedDynamicObj<
+		T extends object,
+		K extends Join<Paths<T, 5>, ''>,
+		KS extends Split<K, '.'>,
+	>(obj: T, key: K): DeepValue<T, KS>
 
-  export function findNestedDynamicObj<T extends object, K extends Join<Paths<T>, ''>, KS extends Split<K, '.'>>(
-    obj: T, key: K
-  ): DeepValue<T, KS>
+	export function stepPrice(price: string | number, step: number): string
 
+	export function generateId(length?: number): string
 
-  export function stepPrice(price: string | number, step: number): string
+	type EnumValue = boolean | string | number
+	type CreateEnumObject = {
+		[key: string]: EnumValue | EnumValue[]
+	}
+	type EnumValues<T> = ValueOf<{
+		[K in keyof T]: T[K]
+	}>
+	type KeyValueEnumObject<T> = {
+		[K in keyof T]: T[K] extends object
+			? T[K] extends any[]
+				? T[K][1]
+				: T[K]
+			: T[K]
+	}
 
+	export function createEnum<
+		T extends CreateEnumObject,
+		T2 extends KeyValueEnumObject<T>,
+	>(
+		obj: T,
+	): T2 & {
+		t: (val: EnumValues<T2>) => EnumValue
+		key: (val: EnumValues<T2>) => string
+		keys: keyof T2
+		map: (
+			callback: (value: EnumValue, key: string, index: number) => void,
+		) => void | keyof T2
+		reduce: <T>(
+			callback: (
+				previousValue: T,
+				value: EnumValue,
+				key: string,
+				index: number,
+			) => void,
+			initialValue: T,
+		) => T
+	}
 
-  export function generateId(length?: number): string
+	export namespace mtime {
+		export function today(): [Moment, Moment]
 
+		export function yesterday(): [Moment, Moment]
 
-  type EnumValue = boolean | string | number
-  type CreateEnumObject = {
-    [key: string]: EnumValue | EnumValue[]
-  }
-  type EnumValues<T> = ValueOf<{
-    [K in keyof T]: T[K]
-  }>
-  type KeyValueEnumObject<T> = {
-    [K in keyof T]: T[K] extends object
-      ? T[K] extends any[]
-        ? T[K][1]
-        : T[K]
-      : T[K]
-  }
+		export function thisWeek(): [Moment, Moment]
 
-  export function createEnum<T extends CreateEnumObject, T2 extends KeyValueEnumObject<T>>(obj: T): T2 & {
-    t: (val: EnumValues<T2>) => EnumValue
-    key: (val: EnumValues<T2>) => string
-    keys: keyof T2
-    map: (callback: (value: EnumValue, key: string, index: number) => void) => void | keyof T2
-    reduce: <T>(callback: (previousValue: T, value: EnumValue, key: string, index: number) => void, initialValue: T) => T
-  }
+		export function pastWeek(): [Moment, Moment]
 
+		export function thisMonth(): [Moment, Moment]
 
-  export namespace mtime {
-    export function today(): [Moment, Moment]
+		export function pastMonth(): [Moment, Moment]
+	}
 
-    export function yesterday(): [Moment, Moment]
+	export function toSimple(text: string): string
 
-    export function thisWeek(): [Moment, Moment]
+	export function toTraditional(text: string): string
 
-    export function pastWeek(): [Moment, Moment]
+	export function checkSameChinese(text1: string, text2: string): boolean
 
-    export function thisMonth(): [Moment, Moment]
+	export function checkIncludeText(text1: string, keyword: string): boolean
 
-    export function pastMonth(): [Moment, Moment]
-  }
+	export function interval(): {
+		start: (callback: () => void, delay?: number) => void
+		stop: () => void
+	}
 
+	export function timeout(): {
+		start: (callback: () => void, delay?: number) => void
+		startSync: (callback: Promise<void>, delay?: number) => void
+		stop: () => void
+	}
 
-  export function toSimple(text: string): string
-
-  export function toTraditional(text: string): string
-
-  export function checkSameChinese(text1: string, text2: string): boolean
-
-  export function checkIncludeText(text1: string, keyword: string): boolean
-
-
-  export function interval(): {
-    start: (callback: () => void, delay?: number) => void
-    stop: () => void
-  }
-
-  export function timeout(): {
-    start: (callback: () => void, delay?: number) => void
-    startSync: (callback: Promise<void>, delay?: number) => void
-    stop: () => void
-  }
-
-  export function aniFrame(): {
-    start: (callback: () => void) => void
-    stop: () => void
-  }
+	export function aniFrame(): {
+		start: (callback: () => void) => void
+		stop: () => void
+	}
 }
 
-declare module "@jsl-react" {
-  import {Dispatch, MutableRefObject, ReactElement, SetStateAction} from "react";
+declare module '@jsl-react' {
+	import {
+		Dispatch,
+		MutableRefObject,
+		ReactElement,
+		SetStateAction,
+	} from 'react'
 
+	export function useSafeState<T>(
+		initialState: T | (() => T),
+	): [T, Dispatch<SetStateAction<T>>]
 
-  export function useSafeState<T>(initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>]
+	export function useTitle(title: string, restoreOnUnmount?: boolean): string
 
+	export function useQueryString<T extends object>(
+		initialState: T,
+	): [T, Dispatch<SetStateAction<T>>]
 
-  export function useTitle(title: string, restoreOnUnmount?: boolean): string
+	export function createBreakpoints<T extends object, K extends keyof T>(
+		screens: T,
+	): {
+		useBreakpoints: (
+			range: K | number,
+			callback: (is: boolean) => void,
+			opts?: { boostrap?: boolean; delay?: number },
+		) => void
+	}
 
+	export function useLocalStorageState<T>(
+		key: string,
+		initialValue: T,
+	): [T, Dispatch<SetStateAction<T>>]
 
-  export function useQueryString<T extends object>(initialState: T): [T, Dispatch<SetStateAction<T>>]
+	export function useSessionStorageState<T>(
+		key: string,
+		initialValue: T,
+	): [T, Dispatch<SetStateAction<T>>]
 
+	export function useToggle<T>(
+		initialState: T | (() => T),
+	): [T, () => void, Dispatch<SetStateAction<T>>]
 
-  export function createBreakpoints<T extends object, K extends keyof T>(screens: T): {
-    useBreakpoints: (
-      range: K | number,
-      callback: (is: boolean) => void,
-      opts?: { boostrap?: boolean, delay?: number }
-    ) => void
-  }
+	// TODO 該類型應該是錯的，這 hook 沒啥用，先寫著放著
+	export function useMethods<
+		T,
+		U extends { [funName: string]: (state: T, ...args: any) => T },
+	>(initialValue: T, methods: U): { [key in keyof T]: (...args: any) => T }
 
+	export function useListEditor<T extends object, K extends keyof T>(
+		initialValueFunc: () => T,
+		init?: boolean,
+	): [
+		T[],
+		Dispatch<SetStateAction<T[]>>,
+		{
+			create: () => void
+			editByIndex: (index: number, value: T) => void
+			editKeyValueByIndex: (index: number, key: K, value: T[K]) => void
+			removeByIndex: (index: number, removeNum?: number) => void
+			removeByKeyValue: (key: K, value: T[K]) => void
+		},
+	]
 
-  export function useLocalStorageState<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>]
+	export function useInitialRef<T>(
+		initialValue: T | (() => T),
+	): MutableRefObject<T>
 
+	// TODO type 沒意外是錯的
+	export function useDebounceFunc(
+		callback: Function[],
+		delay?: number,
+	): [(...args: any) => void, () => void]
 
-  export function useSessionStorageState<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>]
+	export function useDebounce<T>(callback: T, deps: any[], delay?: number): void
 
+	export function useCacheState<T>(
+		symbol: Symbol,
+		initialValue: T,
+	): [T, (arg: T | ((arg: T) => T)) => void]
 
-  export function useToggle<T>(initialState: T | (() => T)): [T, () => void, Dispatch<SetStateAction<T>>]
+	type ServiceProvider = (prop: { children: ReactElement }) => ReactElement
+	type ServiceInject<T> = <V>(getter: (e: T) => V) => V
+	export function createProvider<T>(providerService: () => T): {
+		Provider: ServiceProvider
+		inject: ServiceInject<T>
+	}
 
+	export function createMitt(): {
+		useMitt: {
+			emit: (type: any, event: any) => void
+			on: (type: any, handler: Function) => void
+		}
+	}
 
-  // TODO 該類型應該是錯的，這 hook 沒啥用，先寫著放著
-  export function useMethods<T, U extends { [funName: string]: (state: T, ...args: any) => T }>(initialValue: T, methods: U): { [key in keyof T]: (...args: any) => T }
-
-
-  export function useListEditor<T extends object, K extends keyof T>(initialValueFunc: () => T, init?: boolean): [
-    T[],
-    Dispatch<SetStateAction<T[]>>,
-    {
-      create: () => void
-      editByIndex: (index: number, value: T) => void
-      editKeyValueByIndex: (index: number, key: K, value: T[K]) => void
-      removeByIndex: (index: number, removeNum?: number) => void
-      removeByKeyValue: (key: K, value: T[K]) => void
-    }
-  ]
-
-
-  export function useInitialRef<T>(initialValue: T | (() => T)): MutableRefObject<T>
-
-
-  // TODO 沒意外是錯的
-  export function useDebounceFunc(callback: Function[], delay?: number): [(...args: any) => void, () => void]
-
-
-  export function useDebounce<T>(callback: T, deps: any[], delay?: number): void
-
-
-  export function useCacheState<T>(symbol: Symbol, initialValue: T): [T, (arg: T | ((arg: T) => T)) => void]
-
-
-  export function createProvider<T>(providerService: () => T): {
-    Provider: (prop: { children: ReactElement }) => ReactElement
-    inject: <V>(getter: (e: T) => V) => V
-  }
-
-  export function createMitt(): {
-    useMitt: {
-      emit: any
-      on: any
-    }
-  }
+	export function createI18n<
+		T extends object,
+		K extends keyof T,
+		KS extends Join<Paths<T, 5>, ''>,
+		i18nT extends (nestedKey: KS, replaceArrayStr?: string[]) => string,
+	>(options: {
+		locale: K
+		messages: T
+	}): {
+		t: i18nT
+		Provider: ServiceProvider
+		inject: ServiceInject<{
+			locale: string
+			changeLocale: Dispatch<SetStateAction<string>>
+			t: i18nT
+		}>
+	}
 }
