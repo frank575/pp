ed047b09e3e179cdd189a4f2ac1aa011865b31da
吳俊緯 <wcw850710@gmail.com>
2021-10-21 23:14:40 +0800

    :lipstick: just hello world

--- dart/flutter_app/lib/main.dart
+++ dart/flutter_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,47 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends HookWidget {
-  MyHomePage({required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    final counter = useState(0);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value++,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
       ),
     );
   }
