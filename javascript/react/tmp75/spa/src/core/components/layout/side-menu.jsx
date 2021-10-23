57016fcbf8c2590cd51823c10b0211a4a3edeb29
吳俊緯 <wcw850710@gmail.com>
2021-10-23 13:40:54 +0800

    :art: 僅留下嵌套

--- javascript/react/tmp75/spa/src/core/components/layout/side-menu.jsx
+++ javascript/react/tmp75/spa/src/core/components/layout/side-menu.jsx
@@ -24,26 +24,14 @@ export const SideMenu = () => {
 				inlineCollapsed={menuCollapsed}
 				selectedKeys={selectedKeys}
 			>
-				<Item key={'billboard'} icon={<OrderedListOutlined />}>
-					<Link to={'/billboard'}>公佈欄</Link>
-				</Item>
-				<Item key={'colorful'} icon={<BgColorsOutlined />}>
-					<Link to={'/colorful'}>多主題色</Link>
-				</Item>
-				<Item key={'debug'} icon={<BugOutlined />}>
-					<Link to={'/debug'}>開發調適</Link>
-				</Item>
-				<Item key={'language'} icon={<TranslationOutlined />}>
-					<Link to={'/language'}>多國語系</Link>
-				</Item>
 				<SubMenu key={'nesting'} title={'嵌套選單'}>
 					<SubMenu key={'nesting-1'} title={'第二層'}>
-						<Item key={'billboard'} icon={<ScheduleOutlined />}>
-							<Link to={'/billboard'}>公佈欄(第三層)</Link>
+						<Item key={'nesting-1-1'} icon={<ScheduleOutlined />}>
+							<Link to={'/'}>公佈欄(第三層)</Link>
 						</Item>
 					</SubMenu>
-					<Item key={'billboard'} icon={<OrderedListOutlined />}>
-						<Link to={'/billboard'}>公佈欄(第二層)</Link>
+					<Item key={'nesting-2'} icon={<OrderedListOutlined />}>
+						<Link to={'/'}>公佈欄(第二層)</Link>
 					</Item>
 				</SubMenu>
 			</Menu>
