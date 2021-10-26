ba382043109881f772a21dee807bbee7ff8977a3
吳俊緯 <wcw850710@gmail.com>
2021-10-26 14:11:05 +0800

    :wrench: 新增 light 顏色、fit width、全部 max width -1 px

--- javascript/react/tmp75/spa/tailwind.config.js
+++ javascript/react/tmp75/spa/tailwind.config.js
@@ -4,25 +4,34 @@ module.exports = {
 	important: true,
 	theme: {
 		screens: {
-			'2xl': { max: '1536px' },
-			xl: { max: '1280px' },
-			lg: { max: '1024px' },
-			md: { max: '768px' },
-			sm: { max: '640px' },
-			xs: { max: '425px' },
+			'2xl': { max: '1535px' },
+			xl: { max: '1279px' },
+			lg: { max: '1023px' },
+			md: { max: '767px' },
+			sm: { max: '639px' },
+			xs: { max: '424px' },
 		},
 		extend: {
 			colors: {
 				default: '#000000d9',
+				'light-default': '#0000001a',
 				success: '#52c41a',
+				'light-success': '#52c41a1a',
 				primary: '#1890ff',
+				'light-primary': '#1890ff1a',
 				warning: '#faad14',
+				'light-warning': '#faad141a',
 				danger: '#ff4d4f',
+				'light-danger': '#ff4d4f1a',
 				secondary: '#00000073',
 				disabled: '#00000040',
+				'ant-border': 'rgba(0, 0, 0, 0.06)',
 			},
 			borderWidth: {
 				1: '1px',
+			},
+			width: {
+				fit: 'fit-content',
 			},
 			maxWidth: {
 				'side-menu': '256px',
