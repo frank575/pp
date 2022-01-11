ba55114501ef5bec1f3620d5480f970989f71665
wuchunwei <wcw850710@gmail.com>
2022-01-11 20:08:24 +0800

    feat(xylophone): 新增點擊播放音樂功能

--- ios/xylophone/xylophone/ViewController.swift
+++ ios/xylophone/xylophone/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressButton(_ sender: UIButton) {
+        playSound(resourceName: sender.titleLabel!.text!)
+    }
+    
+    func playSound(resourceName: String) {
+        let url = Bundle.main.url(forResource: resourceName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
