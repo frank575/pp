3e3693f6c241e723806b82f3d9c2748e9d970487
wuchunwei <wcw850710@gmail.com>
2022-01-23 23:04:58 +0800

    feat(main): 新增服務器並加入 /username api

--- javascript/puppeteer/crawler/src/main.js
+++ javascript/puppeteer/crawler/src/main.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer')
-const app = require('express')()
 
 const config = {
+  pageDomain: 'mh',
+  port: 7777,
   puppeteer: {
-    headless: false
+    headless: true
   }
 }
 
@@ -22,17 +23,46 @@ const createGoToPage = async (browser, url) => {
     ...config.puppeteer
   })
 
-  const [minePage, loginPage] = await Promise.all([
-    createGoToPage(browser, 'mh/#/mine'),
-    createGoToPage(browser, 'mh/#/login')
-  ])
+  console.log('頁面開啟，開始登入...')
+
+  const loginPage = await createGoToPage(browser, `${config.pageDomain}/#/login`)
   await loginPage.waitForSelector('#form_username')
   await loginPage.type('#form_username', 'admin')
   await loginPage.type('#form_password', '123456')
   await loginPage.click('button[type=submit]')
+  // await loginPage.waitForResponse(async res => {
+  //   const url = res.url()
+  //   if (url.indexOf('/api/login') !== -1) {
+  //     const json = await res.json()
+  //     if (res.status() === 200) {
+  //       return res.ok()
+  //     }
+  //   }
+  // })
   await loginPage.waitForNavigation()
-  await loginPage.close()
 
-  await waitWithMs(1000)
-  await minePage.reload()
+  console.log('登入完成並關閉登入頁')
+  console.log('開始創建服務器')
+
+  const app = require('express')()
+  app.get('/username', async (req, res) => {
+    const minePage = await createGoToPage(browser, `${config.pageDomain}/#/mine`)
+    const blockSelectorClassName = 'div.flex.items-center.justify-between.px-4.py-2.bg-gray-100.rounded'
+    await minePage.waitForSelector(blockSelectorClassName)
+    await waitWithMs(500)
+    const username = await minePage.evaluate(({ blockSelectorClassName }) => {
+      return document.querySelector(`${blockSelectorClassName} > div > div`)?.textContent || null
+    }, { blockSelectorClassName })
+    await minePage.close()
+    res.json({
+      success: true,
+      message: '取得使用者名稱成功',
+      data: username,
+    })
+  })
+
+  app.listen(config.port, () => {
+    console.log('服務建立完成，請查看服務路徑：')
+    console.log(`http://localhost:${config.port}`)
+  })
 })()
