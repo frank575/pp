8cd9d8044d7fe5593e1ca44e41a3be4ebe8e3b95
wuchunwei <wcw850710@gmail.com>
2022-01-24 22:42:00 +0800

    feat(*): pkg 打包成功

--- javascript/puppeteer/crawler/src/main.js
+++ javascript/puppeteer/crawler/src/main.js
@@ -1,9 +1,34 @@
+const path = require('path')
 const puppeteer = require('puppeteer')
 
+const isPkg = typeof process.pkg !== 'undefined';
+
+//mac path replace
+let chromiumExecutablePath = (isPkg ?
+    puppeteer.executablePath().replace(
+      /^.*?\/node_modules\/puppeteer\/\.local-chromium/,
+      path.join(path.dirname(process.execPath), 'chromium')
+    ) :
+    puppeteer.executablePath()
+);
+
+console.log(process.platform)
+//check win32
+if (process.platform == 'win32') {
+  chromiumExecutablePath = (isPkg ?
+      puppeteer.executablePath().replace(
+        /^.*?\\node_modules\\puppeteer\\\.local-chromium/,
+        path.join(path.dirname(process.execPath), 'chromium')
+      ) :
+      puppeteer.executablePath()
+  );
+}
+
 const config = {
-  pageDomain: 'mh',
+  pageDomain: 'http://test-mh.tc-tech.tw',
   port: 7777,
   puppeteer: {
+    executablePath: chromiumExecutablePath,
     headless: true
   }
 }
