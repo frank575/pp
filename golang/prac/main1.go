8b2820c98814e133d17fb7475d94d5dd7b7e68bf
吳俊緯 <wcw850710@gmail.com>
2022-01-27 15:35:03 +0800

    refactor(prac) 練

--- golang/prac/main1.go
+++ golang/prac/main1.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func beyondHello() {
@@ -43,11 +42,48 @@ func learnTypes() {
 	fmt.Println(*p, *q)
 	m := map[string]int{"three": 3, "four": 4}
 	_, _, _, _, _, _, _, _, _, _ = str, s2, g, f, u, pi, n, a3, s4, bs
-	file, _ := os.Create("output.txt")
-	fmt.Fprint(file, "這句代碼示範如何寫入文件")
-	file.Close()
+	//file, _ := os.Create("output.txt")
+	//fmt.Fprint(file, "這句代碼示範如何寫入文件")
+	//file.Close()
 	fmt.Println(s, c, a4, s3, d2, m)
 	learnFlowControl()
+}
+
+func learnFlowControl() {
+	if true {
+		fmt.Println("這句話肯定被執行")
+	}
+	if false {
+
+	} else {
+	}
+	x := 1
+	switch x {
+	case 0:
+	case 1:
+	case 2:
+	}
+
+	for x := 0; x < 3; x++ {
+		fmt.Println("遍歷", x)
+	}
+
+	for {
+		break
+		continue
+	}
+
+	for key, value := range map[string]int{"one": 1, "two": 2} {
+		fmt.Printf("索引: %s, 值: %d, ", key, value)
+	}
+}
+
+func learnMemory() (p, q *int) {
+	p = new(int)
+	s := make([]int, 20)
+	s[3] = 7
+	r := -2
+	return &s[3], &r
 }
 
 func main() {
