dd85160a06ec9a44d83b37027ba45dc0a698bbb9
wuchunwei <wcw850710@gmail.com>
2022-02-12 17:32:24 +0800

    fear(flutter/begin_prac): 切版 material hello world

--- dart/flutter/begin_prac/lib/main.dart
+++ dart/flutter/begin_prac/lib/main.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({Key? key, required this.title}) : super(key: key);
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue.shade500),
+      child: Row(
+        children: [
+          const IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            tooltip: 'Navigation Menu',
+          ),
+          Expanded(child: title),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.subtitle1,
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Hello world!'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 void main() {
-  runApp(const Center(
-    child: Text(
-      'Hello world!',
-      textDirection: TextDirection.ltr,
-    ),
+  runApp(const MaterialApp(
+    title: 'My App',
+    home: MyScaffold(),
   ));
 }
