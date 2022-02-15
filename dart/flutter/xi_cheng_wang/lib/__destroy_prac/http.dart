4c6e02ad861a845e75bdb971a7ad6b9843915ecb
wuchunwei <wcw850710@gmail.com>
2022-02-15 23:17:54 +0800

    refactor(flutter/xi_chang_wang/prac/http): add CommonModal

--- dart/flutter/xi_cheng_wang/lib/__destroy_prac/http.dart
+++ dart/flutter/xi_cheng_wang/lib/__destroy_prac/http.dart
@@ -1,1 +1,25 @@
 import 'package:http/http.dart' as http;
+
+class CommonModal {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModal(
+      {required this.title,
+      required this.icon,
+      required this.url,
+      required this.statusBarColor,
+      required this.hideAppBar});
+  factory CommonModal.fromJson(Map<String, dynamic> json) {
+    return CommonModal(
+      title: json['title'],
+      icon: json['icon'],
+      url: json['url'],
+      statusBarColor: json['statusBarColor'],
+      hideAppBar: json['hideAppBar'],
+    );
+  }
+}
